84cf09e0f06e0d3df35da2f7c7e5b853d6e40a27
harry <harrisonkuria254@gmail.com>
2025-04-30 18:26:52 +0000

    added images

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,31 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("My first app"),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text("Hello Harry"),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {  },
-        child: Text("+"),
-      ),
-    ),
+    home: Home()
   ));
 }
+
+class Home extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: Text("My first app"),
+      centerTitle: true,
+      backgroundColor: Colors.red[600],
+    ),
+    body: Center(
+      child: Image(image: AssetImage('assets/asset1.jpg'))
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {},
+      child: Text("+"),
+      backgroundColor: Colors.red[600],
+    ),
+  );
+  }
+  }
+
+
